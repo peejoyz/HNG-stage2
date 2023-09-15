f16fa9271fb72984475353ebccc3c2ea61ee922f
Ojo Ayodeji <ayodejipet73@gmail.com>
2023-09-15 20:38:44 +0100

    new deployment

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json())
 
 app.use('/', require('./routes/personRoute'))
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 connectDB().then(() => {
     app.listen(port, () => {
